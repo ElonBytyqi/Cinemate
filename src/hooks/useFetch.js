61f9f144b1b9465e3d076356b5a10c25bc6bbd7a
ElonBytyqi <elon.bytyqi@hotmail.com>
2024-02-08 00:32:10 +0100

    Complete Files

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiPath, queryTerm="") => {
+    const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=ad6b7d4a227586f32328d0fba0011230&query=${queryTerm}`;
+    useEffect(() => {
+        async function fetchMovies(){
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.results);
+        }
+        fetchMovies();
+      }, [url])
+
+  return { data }
+}
