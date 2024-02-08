76dae40a592fe0b3b6b799dc2fcb21b79f541be5
ElonBytyqi <elon.bytyqi@hotmail.com>
2024-02-08 01:05:38 +0100

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,16 @@ export const Footer = () => {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-                <a href="#" className="hover:underline me-4 md:me-6">Instagram</a>
+                <a href="#"  target="_blank" rel="noreferrer" className="hover:underline me-4 md:me-6">Instagram</a>
             </li>
             <li>
-                <a href="#" className="hover:underline me-4 md:me-6"> LinkedIn</a>
+                <a href="#" target="_blank" rel="noreferrer" className="hover:underline me-4 md:me-6"> LinkedIn</a>
             </li>
             <li>
-                <a href="#" className="hover:underline me-4 md:me-6">Youtube</a>
+                <a href="#"  target="_blank" rel="noreferrer"className="hover:underline me-4 md:me-6">Youtube</a>
             </li>
             <li>
-                <a href="#" className="hover:underline">Github</a>
+                <a href="#" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
             </li>
         </ul>
         </div>
